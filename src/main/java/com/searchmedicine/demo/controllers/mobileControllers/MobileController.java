5d5519ad3242a03a26de1dcdfe09c2da245622c2
khalidullayevad <kh.diana0@gmail.com>
2022-02-09 15:43:01 +0600

    PharmacyMedicineDto

--- src/main/java/com/searchmedicine/demo/controllers/mobileControllers/MobileController.java
+++ src/main/java/com/searchmedicine/demo/controllers/mobileControllers/MobileController.java
@@ -1,6 +1,7 @@
 package com.searchmedicine.demo.controllers.mobileControllers;
 
 import com.searchmedicine.demo.entities.dto.CompanyMedicineDto;
+import com.searchmedicine.demo.entities.dto.PharmacyMedicineDto;
 import com.searchmedicine.demo.services.CompanyMedicineService;
 import com.searchmedicine.demo.services.PharmacyMedicineService;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +24,12 @@ public class MobileController {
   private final CompanyMedicineService companyMedicineService;
 
   @GetMapping()
-  public List<CompanyMedicineDto> getMiniFarms() {
-    System.out.println("-----------------------Diana------------------------");
-    System.out.println(companyMedicineService.getAllCompanyMedicine());
+  public List<CompanyMedicineDto> getAllCompanyMedicine() {
     return companyMedicineService.getAllCompanyMedicine();
+  }
+  @GetMapping("/{id}")
+  public List<PharmacyMedicineDto> getPharmacyMedicine(@PathVariable Long id) {
+    return pharmacyMedicineService.getAllCompanyPharmacy(id);
   }
 
 }
--- src/main/java/com/searchmedicine/demo/services/PharmacyMedicineService.java
+++ src/main/java/com/searchmedicine/demo/services/PharmacyMedicineService.java
@@ -1,6 +1,7 @@
 package com.searchmedicine.demo.services;
 
 import com.searchmedicine.demo.entities.dto.CompanyMedicineDto;
+import com.searchmedicine.demo.entities.dto.PharmacyMedicineDto;
 import com.searchmedicine.demo.repositories.PharmacyMedicineRepository;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -11,7 +12,23 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PharmacyMedicineService {
+
   private final PharmacyMedicineRepository pharmacyMedicineRepository;
+  private final EntityManager em;
 
+  public List<PharmacyMedicineDto> getAllCompanyPharmacy(Long id) {
 
+    List<PharmacyMedicineDto> pharmacyCompany = em.createQuery(" select "
+                + " new com.searchmedicine.demo.entities.dto.PharmacyMedicineDto ("
+                + " phc.id,ph.name, m.name, c.name, cm.imageUrl,phc.price )"
+                + " from Medicine m,Company c,Pharmacy ph,PharmacyMedicine phc  "
+                + " inner join CompanyMedicine cm on phc.companyMedicine.id = cm.id "
+                + " where cm.medicine.id = m.id and cm.company.id = c.id "
+                + "and phc.pharmacy.id = ph.id and phc.companyMedicine.id =" + id,
+            PharmacyMedicineDto.class)
+        .getResultList();
+
+    return pharmacyCompany;
+
+  }
 }

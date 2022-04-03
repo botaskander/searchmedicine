66660e625d84d9723ccfccd2c549b66ddc7b5286
real1psy <talshynkb@gmail.com>
2022-04-03 12:21:36 +0600

    fixed error for hibernateLazyInitializer

--- src/main/java/com/searchmedicine/demo/entities/Region.java
+++ src/main/java/com/searchmedicine/demo/entities/Region.java
@@ -1,5 +1,6 @@
 package com.searchmedicine.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "regions")
 public class Region {
     @Id

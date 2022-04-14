32478d643da079f2bf76b25dd95cee1918e9700d
khalidullayevad <kh.diana0@gmail.com>
2022-04-14 15:27:29 +0600

    fixed impl

--- src/main/java/com/searchmedicine/demo/services/MedicineService.java
+++ src/main/java/com/searchmedicine/demo/services/MedicineService.java
@@ -1,9 +1,5 @@
 package com.searchmedicine.demo.services;
 
-import com.searchmedicine.demo.entities.Medicine;
-
-import java.util.List;
-
 public interface MedicineService {
     //Diana Kholidullayeva
 }

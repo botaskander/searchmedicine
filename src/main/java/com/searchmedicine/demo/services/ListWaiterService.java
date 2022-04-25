4408bf0f95bc834f9bbb857fbed0a028713bb870
khalidullayevad <kh.diana0@gmail.com>
2022-04-25 06:39:18 +0600

    fixed notification

--- src/main/java/com/searchmedicine/demo/services/ListWaiterService.java
+++ src/main/java/com/searchmedicine/demo/services/ListWaiterService.java
@@ -3,11 +3,12 @@ package com.searchmedicine.demo.services;
 import com.searchmedicine.demo.entities.ListWaiter;
 import com.searchmedicine.demo.entities.Medicine;
 import com.searchmedicine.demo.entities.Response;
+import com.searchmedicine.demo.entities.Users;
 import java.util.List;
 
 public interface ListWaiterService {
   List<ListWaiter> getNotification(Long medicineId);
-  Response saveListWaiter(Long id);
+  Response saveListWaiter(Long id, Users users);
   List<ListWaiter> getWaiterByUserId(Long id);
   void deleteListWaiter(ListWaiter listWaiter);
   ListWaiter  save(ListWaiter listWaiter);
